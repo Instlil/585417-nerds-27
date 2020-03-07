f95d053c5be82570f754da0e922620cf2edf51d1
Instlil <lzyubina@mail.ru>
2020-03-07 08:35:40 +0300

    всплывающее окно

---
+++ js/popup.js
@@ -0,0 +1,33 @@
+var button = document.querySelector('.contact-tel + .button');
+var popup = document.querySelector('.feedback-modal');
+var close = popup.querySelector('.close-button');
+var form = popup.querySelector('form');
+var login = popup.querySelector('[name=login]');
+var mail = popup.querySelector('[name=email]');
+var feedback = popup.querySelector('[name=letter]');
+
+button.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.add('modal-show');
+    login.focus();
+});
+
+close.addEventListener('click', function (evt){
+    evt.preventDefault();
+    popup.classList.remove('modal-show');
+});
+
+form.addEventListener('submit', function (evt){
+    if(!login.value || !mail.value || !feedback.value){
+        evt.preventDefault();
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
