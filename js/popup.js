6e2b4a9bd1ec802e711e194dc5b4dc6ba9152949
Instlil <lzyubina@mail.ru>
2020-03-10 21:45:39 +0300

    исправления: первая проверка

--- js/popup.js
+++ js/popup.js
@@ -15,19 +15,24 @@ button.addEventListener('click', function (evt) {
 close.addEventListener('click', function (evt){
     evt.preventDefault();
     popup.classList.remove('modal-show');
+    popup.classList.remove('modal-error');
 });
 
 form.addEventListener('submit', function (evt){
     if(!login.value || !mail.value || !feedback.value){
         evt.preventDefault();
+        popup.classList.remove('modal-error');
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add('modal-error');
     }
 });
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
+      if (popup.classList.contains('modal-show')) {
+        popup.classList.remove('modal-show');
+        popup.classList.remove('modal-error');
       }
     }
   });
